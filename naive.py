f1fc6348e7828d3daf9adb2ea88c2c6962cece0e
Rikke Jensen <rikke.myrhoej@gmail.com>
2022-09-24 17:28:15 +0200

    Create framework of suffix tree methods

--- naive.py
+++ naive.py
@@ -14,3 +14,29 @@ class SuffixTree:
 
     def __repr__(self):
         return f'SuffixTree("{self.string}")'
+
+    def find_edge():
+        '''
+        Scans through the children dictionary of a node 
+        Tests each edge for whether it matches the character we are looking for
+        Returns the node if it finds a match. Otherwise returns None'''
+        pass
+
+    def search_edge():
+        '''Should look through edge until end or mismatch'''
+        pass
+
+    def split_edge():
+        '''
+        Splits edge at mismatch position
+        Creates internal node by updating children
+        Returns the new internal node'''
+        pass
+
+    def insert_child():
+        '''Creates a new leaf node and inserts it as a child of a parent node'''
+        pass
+
+    def naive_insert():
+        '''Takes root and string. Returns suffix tree'''
+        pass
