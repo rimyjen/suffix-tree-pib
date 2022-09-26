f3db37000cc38bec31981232c28fcf6db0fcaeab
Rikke Jensen <rikke.myrhoej@gmail.com>
2022-09-26 16:59:03 +0200

    Create search_edge method

--- naive.py
+++ naive.py
@@ -20,9 +20,21 @@ class SuffixTree:
         child = children.get(key)
         return child
 
-    def search_edge():
-        '''Should look through edge until end or mismatch'''
-        pass
+    def search_edge(self, range, j):
+        '''
+        Takes a range of an edge and a suffix index. 
+        If mismatch is found, returns index of mismatch. 
+        If end of range is reached, returns new suffix index.
+        '''
+        i = range[0] #from
+        m = range[1] #to
+        while j <= m:
+            if self.string[i] == self.string[j]:
+                i += 1
+                j += 1
+            else:
+                return i
+        return j
 
     def split_edge():
         '''
