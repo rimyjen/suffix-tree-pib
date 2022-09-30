84f36688ba09bba18381de7f8f7fcc1e8f326c6d
Rikke Jensen <rikke.myrhoej@gmail.com>
2022-09-30 15:25:14 +0200

    Add helper_functions

---
+++ helper_functions.py
@@ -0,0 +1,11 @@
+def search_range(x: str, r: tuple[int,int], y: str, j: int) -> int:
+    """
+    Takes two strings to compare, a range to look through on one string and an index to start at on the other string.
+    Returns how many comparisons were made before either a mismatch or the end of a string was reached.
+    """
+    i, m = r
+    length = min(m-i, len(y)-j)
+    for k in range(length):
+        if x[i+k] != y[j+k]:
+            return k
+    return length
---
+++ tests/test_helper_functions.py
@@ -0,0 +1,23 @@
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..')) #to be able to import modules from parent directory
+
+import pytest
+from helper_functions import *
+
+@pytest.mark.parametrize(
+    'x, r, y, j, expected_value',
+    [("", (0,0), "", 0, 0), 
+    ("a", (0,1), "", 0, 0),
+    ("", (0,0), "a", 0, 0),
+    ("a", (0,1), "a", 0, 1),
+    ("b", (0,1), "a", 0, 0),
+    ("ab", (0,2), "a", 0, 1),
+    ("a", (0,1), "ab", 0, 1),
+    ("ab", (1,2), "b", 0, 1),
+    ("ab", (1,2), "ab", 1, 1)
+    ]
+)
+
+def test_search_range(x, r, y, j, expected_value) -> None:
+    assert search_range(x, r, y, j) == expected_value
