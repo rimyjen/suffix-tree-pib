e2d2f449f1bbf91082a789e2312bf613928bec4d
Rikke Jensen <rikke.myrhoej@gmail.com>
2022-10-02 13:47:41 +0200

    Fix testing to account for sentinel added in class

--- tests/test_naive.py
+++ tests/test_naive.py
@@ -6,28 +6,26 @@ import pytest
 from naive import *
 
 @pytest.fixture
-def string():
-    yield "ababa$"
-
-@pytest.fixture
-def tree(string):
-    yield SuffixTree(string)
+def tree():
+    x = "ababa"
+    yield SuffixTree(x), x + "$"
 
 @pytest.mark.parametrize(
     'key, expected_value',
     [('a', 'child1'), ('b', None)])
 
 def test_find_edge(key, expected_value, tree): 
+    tree = tree[0]
     assert tree.find_edge({'a': 'child1'}, key) == expected_value
 
 def test_search_edge(tree):
+    tree = tree[0]
     assert tree.search_edge(tree.root, 3) == 0
     assert tree.search_edge(SuffixTreeNode((1,3), parent = tree.root), 3) == 2
     assert tree.search_edge(SuffixTreeNode((0,6), parent = tree.root), 4) == 1
 
-def test_search_path(string):
-    x = string
-    tree = SuffixTree(x)
+def test_search_path(tree):
+    tree, x = tree
     v = SuffixTreeNode((0,3), parent = tree.root)
     leaf = SuffixTreeNode((3,6), parent = v, label = 0)
     v.children[x[3]] = leaf
@@ -41,9 +39,8 @@ def test_search_path(string):
     assert tree.search_path(tree.root, 2) == (v, 0) #no matching children after reaching internal node
     assert tree.search_path(tree.root, 4) == (v, 1) #mismatch
     
-def test_split_edge(string):
-    x = string
-    tree = SuffixTree(x)
+def test_split_edge(tree):
+    tree, x = tree
     v = SuffixTreeNode((0,6), parent = tree.root, label = 0)
     tree.root.children[x[0]] = v
     u = tree.split_edge(v, 3)
@@ -55,9 +52,8 @@ def test_split_edge(string):
     assert v.r == (3,6)
     assert v.parent == u
 
-def test_insert_child(string):
-    x = string
-    tree = SuffixTree(x)
+def test_insert_child(tree):
+    tree, x = tree
     u = SuffixTreeNode((0,3), parent = tree.root)
     v = SuffixTreeNode((3,6), parent = u, label = 0)
     u.children[x[3]] = v
